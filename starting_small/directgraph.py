fixed cluster_score computation + replicated performance from before stripping down of code

starting_small/directgraph.py
@@ -16,17 +16,16 @@ class DirectGraph:
     Defines a tensorflow graph of a recurrent neural network
     """
 
-    def __init__(self,
-                 params,
-                 hub,
-                 wx_mat=None,
-                 device=config.Graph.device):
+    def __init__(self, params, hub, wx_mat=None, device=config.Graph.device):
         self.params = params
 
         # placeholders
         self.x = tf.placeholder(tf.int32, [None, None])
         self.y = tf.placeholder(tf.int32, [None, None])
-        self.y_attention = tf.placeholder(tf.float32, shape=[None, None])
+        self.sem_ba_summary = tf.placeholder(tf.float32, shape=())
+        self.syn_ba_summary = tf.placeholder(tf.float32, shape=())
+        self.sem_f1_summary = tf.placeholder(tf.float32, shape=())
+        self.syn_f1_summary = tf.placeholder(tf.float32, shape=())
 
         # weights
         with tf.device('/cpu:0'):  # embedding op works on cpu only
@@ -71,20 +70,15 @@ class DirectGraph:
             final_state, representation = self.rnn_layers(x_embedded)
             # loss
             logit = tf.matmul(final_state, wy) + by
-            attention_3d = tf.tile(tf.expand_dims(self.y_attention, -1), [1, 1, self.params.num_types])
-            eyes = tf.eye(self.params.num_types, self.params.num_types, dtype=tf.float32)
-            y_eyed_3d = tf.nn.embedding_lookup(eyes, self.y)  # [mb, num_y, num_inputs]
-            y_eyed_3d_attended = y_eyed_3d * attention_3d
-            y_eyed_att = tf.reduce_sum(y_eyed_3d_attended, axis=1)  # [mb, num_inputs]
-            att_loss = tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=y_eyed_att)
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=self.y[:, 0])
             # optimizer
             if self.params.optimizer == 'adagrad':
                 optimizer = tf.train.AdagradOptimizer
             else:
                 optimizer = tf.train.GradientDescentOptimizer
+
             # public
-            self.train_step = optimizer(self.params.lr).minimize(tf.reduce_mean(att_loss))
+            self.train_step = optimizer(self.params.lr).minimize(tf.reduce_mean(loss))
             self.softmax_probs = tf.nn.softmax(logit)
             self.pred_ys = tf.argmax(self.softmax_probs, axis=1)
             self.pps = tf.exp(loss)
@@ -102,6 +96,13 @@ class DirectGraph:
                     tf.GraphKeys.GLOBAL_VARIABLES, scope=self.params.flavor + '0')
                 self.wh_adagrad, self.bh_adagrad = self.wh, self.bh  # TODO quick fix
 
+        # tensorboard
+        with tf.device('cpu'):
+            tf.summary.scalar('sem_balanced_accuracy', self.sem_ba_summary)
+            tf.summary.scalar('syn_balanced_accuracy', self.syn_ba_summary)
+            tf.summary.scalar('sem_f1-score', self.sem_f1_summary)
+            tf.summary.scalar('syn_f1-score', self.syn_f1_summary)
+            self.merged = tf.summary.merge_all()
 
     @property
     def cell(self):
@@ -133,7 +134,6 @@ class DirectGraph:
         all_states = []
         final_states = []
         for layer_id in range(self.params.num_layers):
-            print('Making layer {}...'.format(layer_id))
             # prev_layer
             try:
                 prev_layer = all_states[-1]

starting_small/evals.py
@@ -33,17 +33,17 @@ def make_probe_prototype_acts_mat(hub, context_type, graph, sess):
     return res
 
 
-def cluster_score2tb(hub, graph, sess):
-    print('Computing cluster score...')
+def calc_cluster_score(hub, graph, sess, cluster_metric):
+    print('Computing {} score...'.format(cluster_metric))
     # make gold (signal detection masks)
-    num_rows = hub.probe_store.num_types
-    num_cols = hub.probe_store.num_types
+    num_rows = hub.probe_store.num_probes
+    num_cols = hub.probe_store.num_probes
     res = np.zeros((num_rows, num_cols))
     for i in range(num_rows):
         probe1 = hub.probe_store.types[i]
         for j in range(num_cols):
             probe2 = hub.probe_store.types[j]
-            if hub.probe_store.probe_cat_dict[probe1] == hub.probe_store.probe_cat_dict[probe2]:  # TODO test
+            if hub.probe_store.probe_cat_dict[probe1] == hub.probe_store.probe_cat_dict[probe2]:
                 res[i, j] = 1
     gold = res.astype(np.bool)
 
@@ -56,21 +56,6 @@ def cluster_score2tb(hub, graph, sess):
         fn = float(len(np.where((predicted != gold) & (gold == 1))[0]))
         return tp, tn, fp, fn
 
-    # score
-    probe_prototype_acts_mat = make_probe_prototype_acts_mat(hub, 'ordered', graph, sess)
-    probe_sims = cosine_similarity(probe_prototype_acts_mat)
-    calc_signals = partial(calc_signals, probe_sims, gold)
-    sims_mean = np.asscalar(np.mean(probe_sims))
-    res = calc_cluster_score(calc_signals, sims_mean, verbose=False)
-
-    print('cluster_score={}'.format(res))  # TODO test
-
-    # TODO send everythign to tensorboard
-
-    return res
-
-
-def calc_cluster_score(calc_signals, sims_mean, verbose=True):
     def calc_probes_fs(thr):
         tp, tn, fp, fn = calc_signals(thr)
         precision = np.divide(tp + 1e-10, (tp + fp + 1e-10))
@@ -101,21 +86,26 @@ def calc_cluster_score(calc_signals, sims_mean, verbose=True):
         ba = (sensitivity + specificity) / 2  # balanced accuracy
         return ba
 
+    # sims
+    probe_prototype_acts_mat = make_probe_prototype_acts_mat(hub, 'ordered', graph, sess)
+    probe_sims = cosine_similarity(probe_prototype_acts_mat)
+    calc_signals = partial(calc_signals, probe_sims, gold)
+    sims_mean = np.mean(probe_sims).item()
     # use bayes optimization to find best_thr
-    print('Finding best thresholds between using bayesian-optimization...')
+    print('Finding best threshold using bayesian-optimization...')
     gp_params = {"alpha": 1e-5, "n_restarts_optimizer": 2}
-    if config.Saver.matching_metric == 'F1':
+    if cluster_metric == 'f1':
         fun = calc_probes_fs
-    elif config.Saver.matching_metric == 'BalAcc':
+    elif cluster_metric == 'ba':
         fun = calc_probes_ba
-    elif config.Saver.matching_metric == 'CohensKappa':
+    elif cluster_metric == 'ck':
         fun = calc_probes_ck
     else:
         raise AttributeError('rnnlab: Invalid arg to "metric".')
-    bo = BayesianOptimization(fun, {'thr': (-1.0, 1.0)}, verbose=verbose)
+    bo = BayesianOptimization(fun, {'thr': (-1.0, 1.0)}, verbose=False)
     bo.explore(
         {'thr': [sims_mean]})  # keep bayes-opt at version 0.6 because 1.0 occasionally returns 0.50 wrongly
-    bo.maximize(init_points=2, n_iter=config.Saver.num_opt_steps,
+    bo.maximize(init_points=2, n_iter=config.Eval.num_opt_steps,
                 acq="poi", xi=0.01, **gp_params)  # smaller xi: exploitation
     best_thr = bo.res['max']['max_params']['thr']
     # use best_thr
@@ -126,7 +116,7 @@ def calc_cluster_score(calc_signals, sims_mean, verbose=True):
 
 def term_sims2tb(hub, graph, sess):
     print('Making prototype term activations...')
-    term_acts_mat_sum = np.zeros((self.hub.params.num_types, self.params.embed_size))
+    term_acts_mat_sum = np.zeros((hub.params.num_types, hub.params.embed_size))
     # collect acts
     pbar = pyprind.ProgBar(hub.num_mbs_in_token_ids)
     for (x, y) in hub.gen_ids(num_iterations=1):
@@ -152,7 +142,7 @@ def term_sims2tb(hub, graph, sess):
         print('Found {} Nans in term sim_mat'.format(num_nans), 'red')
 
 
-def calc_pp(hub, graph, sess, is_test):
+def calc_pp(hub, graph, sess, timepoint, is_test):
     print('Calculating {} perplexity...'.format('test' if is_test else 'train'))
     pp_sum, num_batches, pp = 0, 0, 0
     pbar = pyprind.ProgBar(hub.num_mbs_in_token_ids)
@@ -162,4 +152,9 @@ def calc_pp(hub, graph, sess, is_test):
         pp_sum += pp_batch
         num_batches += 1
     pp = pp_sum / num_batches
+    print('pp={}'.format(pp))
+
+    # TODO sent to tensorboard
+
+
     return pp

starting_small/jobs.py
@@ -3,15 +3,17 @@ import time
 from scipy.stats import bernoulli
 import numpy as np
 import pyprind
+from shutil import copyfile
 
 from childeshub.hub import Hub
 
 from starting_small import config
-from starting_small.evals import cluster_score2tb, calc_pp, term_sims2tb
+from starting_small.evals import calc_cluster_score, calc_pp, term_sims2tb
 from starting_small.directgraph import DirectGraph
+from starting_small.params import ObjectView
 
 
-def rnn_job(params):
+def rnn_job(param2val):
     def train_on_corpus(data_mb, train_mb, train_mb_generator, graph, sess):
         print('Training on items from mb {:,} to mb {:,}...'.format(train_mb, data_mb))
         pbar = pyprind.ProgBar(data_mb - train_mb)
@@ -23,28 +25,29 @@ def rnn_job(params):
             if data_mb == train_mb:
                 return train_mb
 
-    def evaluate(hub, mb_name, graph, sess):
+    def evaluate(hub, graph, sess, summary_writer, timepoint):
         print('Evaluating...')
-        for mode in config.Saver.hub_modes:
+        # term_sims2tb(hub, graph, sess)  # TODO perhaps compute probe_sims from this?
+        feed_dict = {}
+        for mode, (ba_plh, f1_plh) in zip(config.Eval.hub_modes,
+                                     [(graph.sem_ba_summary, graph.sem_f1_summary),
+                                      (graph.syn_ba_summary, graph.syn_f1_summary)]):
             hub.switch_mode(mode)
-            calc_pp(hub, graph, sess, is_test=True)
-            cluster_score2tb(mb_name, graph, sess)
-            # term_sims2tb(hub, graph, sess)   # TODO perhaps compute probe_sims from this?
+            calc_pp(hub, graph, sess, timepoint, is_test=True)  # TODO send to tb every batch?
+            feed_dict[ba_plh] = calc_cluster_score(hub, graph, sess, 'ba')
+            feed_dict[f1_plh] = calc_cluster_score(hub, graph, sess, 'f1')
+            #
+        summary = sess.run(graph.merged, feed_dict=feed_dict)
+        summary_writer.add_summary(summary, timepoint)
 
     def make_reinit_timepoints(params):
         if params.reinit.split('_')[0] == 'all':
             result = range(params.num_saves)
         elif params.reinit.split('_')[0] == 'mid':
             result = [params.num_saves // 2]
-        elif params.reinit.split('_')[0] == 'none':
-            result = []
         else:
             raise AttributeError('starting_small: Invalid arg to "reinit".')
         return result
-
-    def to_mb_name(mb):
-        zeros = '0' * 9
-        return (zeros + str(mb))[-len(zeros):]
 
     def reinit_weights(graph, sess, params):
         print('Reinitializing with reinit={}'.format(params.reinit))
@@ -77,31 +80,73 @@ def rnn_job(params):
         graph.wh_adagrad.load_params_d(wh_adagrad, session=sess)
         graph.bh_adagrad.load_params_d(bh_adagrad, session=sess)
 
-    print(params, flush=True)
+    params = ObjectView(param2val)
+    params.num_y = 1
     hub = Hub(params=params)
     g = tf.Graph()
     with g.as_default():
+        # graph + tensorflow
         graph = DirectGraph(params, hub)
+        tb_p = config.Dirs.tensorboard / param2val['job_name']
+        if not tb_p.exists():
+            tb_p.mkdir(parents=True)
+        else:
+            tb_p.unlink()  # clear previous data
         sess = tf.Session()
+        summary_writer = tf.summary.FileWriter(tb_p, sess.graph)
         sess.run(tf.global_variables_initializer())
         # train and save
         train_mb = 0
         train_mb_generator = hub.gen_ids()  # has to be created once
         start_train = time.time()
         for timepoint, data_mb in enumerate(hub.data_mbs):
-            mb_name = to_mb_name(data_mb)
             if timepoint == 0:
                 # save
-                evaluate(hub, mb_name, graph, sess)
+                evaluate(hub, graph, sess, summary_writer, timepoint)
             else:
                 # train + save
                 train_mb = train_on_corpus(data_mb, train_mb, train_mb_generator, graph, sess)
-                evaluate(hub, mb_name, graph, sess)
+                evaluate(hub, graph, sess, summary_writer, timepoint)
             print('Completed Timepoint: {}/{} |Elapsed: {:>2} mins\n'.format(
                 timepoint, hub.params.num_saves, int(float(time.time() - start_train) / 60)))
             # reinitialize recurrent weights
-            if timepoint in make_reinit_timepoints(params):
-                reinit_weights(graph, sess, params)
+            if params.reinit is not None:
+                if timepoint in make_reinit_timepoints(params):
+                    reinit_weights(graph, sess, params)
         sess.close()
 
 
+def backup_job(param_name, job_name, allow_rewrite):
+    """
+    function is not imported from ludwigcluster because this would require dependency on worker.
+    this informs LudwigCluster that training has completed (backup is only called after training completion)
+    copies all data created during training to backup_dir.
+    Uses custom copytree fxn to avoid permission errors when updating permissions with shutil.copytree.
+    Copying permissions can be problematic on smb/cifs type backup drive.
+    """
+    src = config.Dirs.runs / param_name / job_name
+    dst = config.Dirs.backup / param_name / job_name
+    if not dst.parent.exists():
+        dst.parent.mkdir(parents=True)
+    copyfile(str(config.Dirs.runs / param_name / 'param2val.yaml'),
+             str(config.Dirs.backup / param_name / 'param2val.yaml'))  # need to copy param2val.yaml
+
+    def copytree(s, d):
+        d.mkdir(exist_ok=allow_rewrite)  # set exist_ok=True if dst is partially missing files whcih exist in src
+        for i in s.iterdir():
+            s_i = s / i.name
+            d_i = d / i.name
+            if s_i.is_dir():
+                copytree(s_i, d_i)
+            else:
+                copyfile(str(s_i), str(d_i))  # copyfile works because it doesn't update any permissions
+    # copy
+    print('Backing up data...  DO NOT INTERRUPT!')
+    try:
+        copytree(src, dst)
+    except PermissionError:
+        print('Backup failed. Permission denied.')
+    except FileExistsError:
+        print('Already backed up {}'.format(dst))
+    else:
+        print('Backed up data to {}'.format(dst))

starting_small/params.py
@@ -1,126 +1,28 @@
-import pandas as pd
-import numpy as np
 
-from starting_small import config
+
+class ObjectView(object):
+    def __init__(self, d):
+        self.__dict__ = d
 
 
 class Params:
-    options = [('num_parts', 256, [[1, 2, 4, 8, 256, 512, 1024]]),
-               ('corpus_name', 'childes-20180319', [['childes-20171212', 'childes-20171213',
-                                                     'childes-20180120', 'childes-20180315',
-                                                     'childes-20180319']]),
-               ('sem_probes_name', 'semantic-raw', [['semantic-reduced',
-                                                     'semantic-lemma',
-                                                     'semantic-raw',
-                                                     'semantic-early',
-                                                     'semantic-late']]),
-               ('syn_probes_name', 'syntactic-mcdi', [['syntactic-mcdi',
-                                                       'syntactic-mcdi',
-                                                       'syntactic-mcdi']]),
-               ('num_types', 4096, [[32768, 16384, 8192, 4096, 2048, 1024]]),
-               ('part_order', 'inc_age', [['inc', 'dec', 'shuffled', 'unordered', 'middec', 'midinc'],
-                                           ['age', 'punctuation', 'noun', 'pronoun', 'determiner', 'interjection',
-                                            'preposition', '3-gram', '1-gram', 'entropy',
-                                            'dec_noun+punctuation',
-                                            'inc_noun+punctuation',
-                                            'nouns-context-entropy-1-left',
-                                            'punctuations-context-entropy-1-left',
-                                            'probes-context-entropy-1-left',
-                                            'conjunctions-context-entropy-1-right',
-                                            'prepositions-context-entropy-1-right',
-                                            'verbs-context-entropy-1-right']]),
-               ('num_iterations', 20, [[1, 2, 3, 4, 5, 10, 20]]),
-               ('reinit', 'none_10_w', [['none', 'mid', 'all'],
-                                        [10, 50, 60, 60, 80, 90, 100],
-                                        ['w', 'a', 'w+a', 'b', 'w+b']]),  # w=weights, a=adagradm b=bias
-               ('num_saves', 10, [[1, 5, 10, 20]]),
-               ('bptt_steps', 7, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]),
-               ('num_layers', 1, [[1, 2, 3, 4]]),
-               ('rep_layer_id', 0, [[0, 1, 2, 3]]),
-               ('mb_size', 64, [[1, 2, 4, 8, 16, 32, 64, 128, 256, 1024]]),
-               ('lr', 0.01, [[0.001, 0.002, 0.004, 0.006, 0.008, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.3, 0.6]]),
-               ('flavor', 'rnn', [['rnn', 'lstm', 'deltarnn', 'fahlmanrnn']]),
-               ('optimizer', 'adagrad', [['sgd', 'adagrad']]),
-               ('embed_size', 512, [[2, 64, 128, 256, 512]]),
-               ('wx_init', 'random', [['random', 'glove300']]),
-               ]
+    num_parts = [256]
+    corpus_name = ['childes-20180319']
+    sem_probes_name = ['childes-20180319_4096']
+    syn_probes_name = ['childes-20180319_4096']
+    num_types = [4096]
+    part_order = ['inc_age', 'dec_age', 'shuffled_age', 'unordered_age', 'midinc_age', 'middec_age']
+    num_iterations = [20]
+    reinit = [None, 'all_10_w', 'all_10_a', 'all_90_w', 'all_90_a']  # w=weights, a=adagrad b=bias
+    num_saves = [10]
+    bptt_steps = [7, 14]
+    num_layers = [1]
+    rep_layer_id = [0]
+    mb_size = [64]
+    lr = [0.01]
+    flavor = ['rnn']  # rnn, lstm, deltarnn, fahlmanrnn
+    optimizer = ['adagrad']
+    embed_size = [512]
+    wx_init = ['random', 'glove300']
 
-    default_dict = {o[0]: o[1] for o in options}
-
-    def __init__(self, params_df_row=None):
-        self.params_d = self.load_params_d(params_df_row) or self.default_dict
-
-    @property
-    def dict(self):
-        res = self.default_dict.copy()
-        res.update(self.params_d)
-        return res
-
-    def __getattr__(self, name):
-        if name in self.dict:
-            return self.dict[name]
-        else:
-            raise AttributeError("No such attribute: " + name)
-
-    def __str__(self):
-        res = []
-        for k, v in sorted(self.params_d.items()):
-            res.append('{:<16}: {}'.format(k, v))
-        return '=' * 40 + '\n' + '\n'.join(res) + '\n' + '=' * 40 + '\n'
-
-    def load_params_d(self, params_df_row):
-        if params_df_row is None:
-            return False
-        res = params_df_row.to_dict()
-        for k, v in res.items():
-            if isinstance(v, np.int64):
-                res[k] = int(v)
-        self.check_validity(res)
-        return res
-
-    def check_validity(self, new_params_d):
-        excluded_from_validity_check = ['model_name', 'backup_dir', 'runs_dir']
-        name2options = {o[0]: o[2] for o in self.options}
-        e = None
-        # check if in options
-        for k, v in new_params_d.items():
-            if k in excluded_from_validity_check:
-                continue
-            parts = str(v).split('_')
-            allowed_parts_list = name2options[k]
-            if not len(parts) == len(allowed_parts_list):
-                e = 'Missing parts of "{}".'.format(k)
-            for part_id, (part, allowed_parts) in enumerate(zip(parts, allowed_parts_list)):
-                if part not in [str(p) for p in allowed_parts]:
-                    e = 'Part {} of "{}" must be in {}'.format(part_id + 1, k, allowed_parts)
-        # syn2sem
-        if ('l2m' in new_params_d['syn2sem']
-            or 'm2l' in new_params_d['syn2sem']) \
-                and 'hard' in new_params_d['syn2sem']:
-            e = 'Cannot use "hard" in combination with "l2m" or "m2l".'
-        if new_params_d['syn2sem'] != 'dist_least_hard' \
-                and 'num_y' not in new_params_d:
-            e = 'starting_small: Did you forget to adjust "num_y"?'
-        # do not allow None
-        if any([True if new_config is None else False for new_config in new_params_d]):
-            e = 'starting_small: "None" is not allowed in configs file.'
-        # bptt_steps
-        if 'bptt_steps' in new_params_d \
-                and 'start_bptt' not in new_params_d:
-            e = 'starting_small: Must specify "{}" and "{}" in tandem.'.format('bptt_steps', 'start_bptt')
-        # throw error
-        if e is not None:
-            raise Exception(e)
-
-
-# params_df = experimental params + defaults
-p = config.Dirs.root / 'params.csv'
-without_defaults_df = pd.read_csv(p, index_col=False)
-num_rows = len(without_defaults_df)
-d = {}
-for k, v, _ in Params.options:
-    try:
-        d[k] = without_defaults_df[k]
-    except KeyError:
-        d[k] = [v] * num_rows
-params_df = pd.DataFrame.from_dict(data=d)
+    # TODO need param that specifies num_iterations specific to a partition e.g. 30, 10 for part1 and part2