a7c1efae925b1b4e99fe6c1ae0a656799dce72a5
Philip Huebner <info@philhuebner.com>
2019-10-24 10:07:07 -0500

    Update figs.py

--- startingsmall/figs.py
+++ startingsmall/figs.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import seaborn as sns
 import numpy as np
+from typing import List, Tuple, Union
 
 from startingsmall import config
 
@@ -14,16 +15,23 @@ def human_format(num, pos):  # pos is required for formatting mpl axis ticklabel
     return '{}{}'.format(num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
 
 
-def make_summary_trajs_fig(summary_data, traj_name, title=None, palette_ids=None,
-                           figsize=None, ylims=None, reverse_colors=False, y_grid=False,
-                           plot_max_line=False, plot_max_lines=False, alternative_labels=None,
-                           vlines=None, label_n=True):
+def make_summary_fig(summaries: list,
+                     ylabel: str,
+                     title: str = 'Un-titled',
+                     palette_ids: List[int] = None,
+                     figsize: Tuple[int, int] = None,
+                     ylims: List[float] = None,
+                     y_grid: bool = False,
+                     plot_max_line: bool = False,
+                     plot_max_lines: bool = False,
+                     alternative_labels: Union[None, list] = None,
+                     vlines: List[int] = None,
+                     ):
     # fig
     fig, ax = plt.subplots(figsize=figsize)
-    if title is not None:
-        plt.title(title)
+    plt.title(title)
     ax.set_xlabel('Mini Batch', fontsize=config.Figs.axlabel_fs)
-    ax.set_ylabel(traj_name + '\n+/- Std Dev', fontsize=config.Figs.axlabel_fs)
+    ax.set_ylabel(ylabel + '\n+/- Std Dev', fontsize=config.Figs.axlabel_fs)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.tick_params(axis='both', which='both', top=False, right=False)
@@ -34,26 +42,35 @@ def make_summary_trajs_fig(summary_data, traj_name, title=None, palette_ids=None
         ax.set_ylim(ylims)
 
     # palette
-    num_summaries = len(summary_data)
-    if reverse_colors:
-        palette = np.asarray(sns.color_palette('hls', num_summaries)[::-1])
-    else:
-        palette = np.asarray(sns.color_palette('hls', num_summaries))
+    num_summaries = len(summaries)
+    palette = np.asarray(sns.color_palette('hls', num_summaries))
     if palette_ids is not None:
         colors = iter(palette[palette_ids])
     else:
         colors = iter(palette)
 
+    if alternative_labels is not None:
+        alternative_labels = iter(alternative_labels)
+
     # plot summary
     max_ys = []
-    for summary_data_id, (x, mean_traj, std_traj, label, n) in enumerate(summary_data):
-        max_ys.append(max(mean_traj))
+    for x, y_mean, y_std, label, n in summaries:
+        max_ys.append(max(y_mean))
+
         if alternative_labels is not None:
-            label = next(alternative_labels)
-            label += '\nn={}'.format(n) if label_n else ''
-        ax.plot(x, mean_traj, '-', linewidth=config.Figs.lw, color=next(colors),
+            try:
+                label = next(alternative_labels)
+            except StopIteration:
+                raise ValueError('Not enough values in ALTERNATIVE_LABELS')
+
+        try:
+            color = next(colors)
+        except StopIteration:
+            raise ValueError('Not enough values in PALETTE_IDS')
+
+        ax.plot(x, y_mean, '-', linewidth=config.Figs.lw, color=color,
                 label=label, zorder=3 if n == 8 else 2)
-        ax.fill_between(x, mean_traj + std_traj, mean_traj - std_traj, alpha=0.5, color='grey')
+        ax.fill_between(x, y_mean + y_std, y_mean - y_std, alpha=0.5, color='grey')
 
     # legend
     if title:
