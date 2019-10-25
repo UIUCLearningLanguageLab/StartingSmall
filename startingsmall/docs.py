962db68564177d68e99bb2cb83eb4f85b1d285d0
phueb <info@philhuebner.com>
2019-10-25 13:52:02 -0500

    chose test docs from middle of corpus

--- startingsmall/docs.py
+++ startingsmall/docs.py
@@ -1,9 +1,17 @@
 import random
+from typing import List, Union
 
 from startingsmall import config
 
 
-def load_docs(params, num_test_docs=100, shuffle_seed=20, split_seed=3):
+childes_mid_doc_ids = tuple(range(1500, 1600))
+
+
+def load_docs(params,
+              test_doc_ids: Union[List[int], None] = childes_mid_doc_ids,
+              num_test_docs=100,
+              shuffle_seed=20,
+              split_seed=3):
     """
     100 test docs + random seed = 3 were used in PH master's thesis
     """
@@ -20,9 +28,13 @@ def load_docs(params, num_test_docs=100, shuffle_seed=20, split_seed=3):
 
     # split train/test
     print('Splitting docs into train and test...')
-    num_test_doc_ids = num_docs - num_test_docs
-    random.seed(split_seed)
-    test_doc_ids = random.sample(range(num_test_doc_ids), num_test_docs)
+    if test_doc_ids is None:
+        num_test_doc_ids = num_docs - num_test_docs
+        random.seed(split_seed)
+        test_doc_ids = random.sample(range(num_test_doc_ids), num_test_docs)
+    else:
+        test_doc_ids = test_doc_ids
+
     test_docs = []
     for test_line_id in test_doc_ids:
         test_doc = docs.pop(test_line_id)  # removes line and returns removed line
